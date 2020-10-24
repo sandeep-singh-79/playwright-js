bd8a09f5017335db95321172c3cf0541f96a7084
Sandeep Singh <s.sandeep79@ymail.com>
2020-10-24 19:10:51 +0530

    navigate to another page

--- playwright1.js
+++ playwright1.js
@@ -3,8 +3,13 @@ const playwright = require('playwright');
 (async () => {
   const browser = await playwright.chromium.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.github.com');
-  await page.screenshot({ path: 'github-home.png' });
+
+  const modal_button_selector = '.modal-footer > button'
+  await page.goto('https://native-land.ca/');
+  await page.waitForSelector(modal_button_selector)
+  await page.click(modal_button_selector)
+
+  await page.screenshot({ path: 'example.png' });
 
   await browser.close();
 })();
