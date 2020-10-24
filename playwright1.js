f19ace1ba44a96e1fabfc90d8d452bcb4e1dc2d8
Sandeep Singh <s.sandeep79@ymail.com>
2020-10-24 19:47:10 +0530

    getting results

--- playwright1.js
+++ playwright1.js
@@ -7,6 +7,7 @@ const playwright = require('playwright');
   const modal_button_selector = '.modal-footer > button'
   const search_selector = 'input[placeholder=Search]'
   const location_selector = 'li.active > a'
+  const results_selector = '.results-tab'
 
   with(page) {
     await goto("https://native-land.ca/");
@@ -16,6 +17,13 @@ const playwright = require('playwright');
     await click(search_selector)
     await keyboard.type('Philadelphia')
     await waitForSelector(location_selector)
+
+    await click(location_selector)
+    await waitForSelector(`${results_selector} > p`)
+
+    const results = await $(results_selector)
+    const text = await results.evaluate(element => element.innerText)
+    console.log(text)
 
     await screenshot({ path: "example.png" });
   }
