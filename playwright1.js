54db2546f5165bd66d5dcbbe5defba44d7d9bc57
Sandeep Singh <s.sandeep79@ymail.com>
2020-10-24 13:50:16 +0530

    go to a page

--- playwright1.js
+++ playwright1.js
@@ -2,6 +2,9 @@ const browser = require('playwright')
 
 (await () => {
         const chrome = await browser.chromium.launch()
+        const page = await chrome.newPage()
+        await page.goto('https://github.com')
+        await page.screenshot({path: `github_home.png`})
         await chrome.close()
     }
 );
