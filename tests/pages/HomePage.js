dccb17e5c85acda9bb99a7fe79107d6a8749a901
Sandeep Singh <s.sandeep79@ymail.com>
2021-01-10 16:22:49 +0530

    refactoring initiated

--- tests/pages/HomePage.js
+++ tests/pages/HomePage.js
@@ -1,10 +1,27 @@
 class HomePage {
   constructor(page) {
     this.page = page;
+    this.modal_button_selector = ".modal-footer > button";
+    this.search_selector = "input[placeholder=Search]";
+    this.location_selector = "li.active > a";
+    this.results_selector = ".results-tab";
   }
 
   async navigateTo() {
     await this.page.goto("https://native-land.ca/");
+  }
+
+  async dismissModalBox() {
+    await this.page.waitForSelector(this.modal_button_selector);
+    await this.page.click(this.modal_button_selector);
+  }
+
+  async searchForCityPhiladelphia() {
+    await this.page.click(this.search_selector);
+    await this.page.keyboard.type("Philadelphia");
+    await this.page.waitForSelector(this.location_selector);
+
+    await this.page.click(this.location_selector);
   }
 }
 
--- tests/tests/playwright1.js
+++ tests/tests/playwright1.js
@@ -9,22 +9,15 @@ const { HomePage } = require("../pages/HomePage");
   });
   const page = await browser.newPage();
   const homePage = new HomePage(page);
-
-  const modal_button_selector = ".modal-footer > button";
-  const search_selector = "input[placeholder=Search]";
-  const location_selector = "li.active > a";
+  
   const results_selector = ".results-tab";
 
+  homePage.navigateTo();
+  homePage.dismissModalBox();
+  homePage.searchForCityPhiladelphia(); 
+
+
   with (page) {
-    homePage.navigateTo();
-    await waitForSelector(modal_button_selector);
-    await click(modal_button_selector);
-
-    await click(search_selector);
-    await keyboard.type("Philadelphia");
-    await waitForSelector(location_selector);
-
-    await click(location_selector);
     await waitForSelector(`${results_selector} > p`);
 
     const results = await $(results_selector);
